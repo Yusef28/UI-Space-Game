0b036e9ea75d3f24cea2b2453698c4da772a95a0
Yusef <yusef28@my.yorku.ca>
2016-04-30 17:07:45 -0400

    Added planets and Star Systems
    Random gen between 4 and 14 for planets and then again for star systems,
    and partially random gen features for each of each.
    
    What I need to work on next is the UI. Simply, I want to animate the
    flipping between screens.

--- AngularApp4/app4.js
+++ AngularApp4/app4.js
@@ -1,15 +1,19 @@
 var app = angular.module('glist', []);
 
-app.controller("gctrl", function($scope, $interval){
+app.controller("gctrl", function($scope, $interval, $timeout){
 
 	$scope.counter = 0;
 	
 	var promise
+	
+	//Player Variables
 	$scope.inter = 50;
 	$scope.fuel = {name: 'Basic', multiplier: 1, tank:100, loss:0.1};
-	$scope.speed = $scope.inter * $scope.fuel.multiplier;
+	$scope.speed = $scope.inter / $scope.fuel.multiplier;
 	$scope.fueldisplay = -1;
 	
+	
+	//UI Functions
 	$scope.showFuel = function(i){
 	$scope.fueldisplay = i;
 	}
@@ -32,6 +36,8 @@ app.controller("gctrl", function($scope, $interval){
 	$scope.stop();
 	});
 	
+	
+	//Game Loop Basically
 	function count(){
 	$scope.counter++;//miles from home
 	$scope.fuel.tank -= $scope.fuel.loss;
@@ -40,6 +46,10 @@ app.controller("gctrl", function($scope, $interval){
 	for(i=0; i<$scope.planets.length; i++)
 		{
 			$scope.planets[i].distancefrom = $scope.planets[i].distance - $scope.counter;
+			if($scope.planets[i].stopping == true && $scope.planets[i].distancefrom <= 20){
+				$scope.gatherFuel($scope.planets[i]);
+				$scope.planets.splice(i, 1);
+			}
 			
 			if($scope.planets[i].distancefrom < 0){
 			$scope.planets.splice(i, 1);
@@ -47,40 +57,73 @@ app.controller("gctrl", function($scope, $interval){
 		}
 	}
 	
+	
+	//function to adjust fuel and player variables
+	$scope.gatherFuel = function(planet)
+	{
+	$scope.stop();
+	$timeout(function(){$scope.start()}, 3000);
+	$scope.fuel.name = planet.fuel.type;
+	$scope.fuel.multiplier = planet.fuel.mult;
+	$scope.fuel.tank = 100;
+	$scope.fuel.loss = planet.fuel.loss
+	$scope.speed = $scope.inter / $scope.fuel.multiplier;
+	};
+
+	
 	//maybe don't need since galaxies does this
 	//$scope.universe = [];
 	
 	//$scope.galaxies = [];
-	//$scope.starsystems = [];
+	$scope.starsystems = [];
 	$scope.planets = [];
 	
 	//$scope.createGalaxies = function(){};
 	//$scope.createStarSystems = function(){};
 	
-	$scope.createPlanets = function(){
+	$scope.createPlanets = function(ssp, ssn){//add ss
 
-		var planetNum = Math.floor(Math.random() * 10);//num of planets
-		var SSnum = 0;
+		var planetNum = Math.floor(Math.random() * 10) +4;//num of planets
+		var PSSnum = 1 ;
 		
 		for(i= 0; i < planetNum; i++){
-			SSnum = i;
+			PSSnum = i ;//ones column, tens column
 			var planet = {
-			name: i,
-			distance: i*500 + Math.random()*200,
+			name: ssn +''+ i,
+			distance: PSSnum*500 + Math.random()*200 + 500*ssn,
 			distancefrom: 0,
-			fuel: { type:'fuel type',
-					price: Math.floor(Math.random()*50)+SSnum*50,
-					loss: Math.floor(Math.random()*SSnum)/10,
-					mult: Math.floor(Math.random()*SSnum)
+			stopping: false,
+			fuel: { type:'Basic',
+					price: Math.floor(Math.random()*50)+PSSnum*50,
+					loss: 0.1 * Math.random()+0.01,
+					mult: Math.floor(Math.random()*PSSnum)+1
 					}
 				}
-			$scope.planets.push(planet);		
+			planet.distancefrom = planet.distance;
+			$scope.planets.push(planet);
+			ssp.push(planet);	//this?		
 			}
 	
 	
 		};
-
 	
-	$scope.createPlanets();
+	$scope.createStarSystems = function(){
+
+		var SSnum = Math.floor(Math.random() * 10) +4;//num of planets
+		
+		for(k = 0; k < SSnum; k++){//had to use different var here
+		var starsystem = {
+			name: k, 
+			distance: 700 * 15 * k,
+			inThisSS: false,
+			planets: []}
+		
+			$scope.createPlanets(starsystem.planets, k);
+		$scope.starsystems.push(starsystem);
+	
+		}
+	}
+	
+	$scope.createStarSystems();
 
 });
